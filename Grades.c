459e61dfde2116a6d165685a148bf74d99ebebff
gunasrip <119730782+gunasrip@users.noreply.github.com>
2023-01-05 00:36:53 +0530

    #include<stdio.h> int main() {     int a,b,c,d,e;     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);     int p;     p=(a+b+c+d+e)/5;     if(p>=90)     {         printf("Grade A");     }     else if(p>=80)     {         printf("Grade B");     }     else if(p>=70)     {         printf("Grade C");     }     else if(p>=60)     {         printf("Grade D");     }     else if(p>=40)     {         printf("Grade E");     }     else if(p<40)     {         printf("Grade F");     }     else     {         printf("1");     } }

---
+++ Grades.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e;
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    int p;
+    p=(a+b+c+d+e)/5;
+    if(p>=90)
+    {
+        printf("Grade A");
+    }
+    else if(p>=80)
+    {
+        printf("Grade B");
+    }
+    else if(p>=70)
+    {
+        printf("Grade C");
+    }
+    else if(p>=60)
+    {
+        printf("Grade D");
+    }
+    else if(p>=40)
+    {
+        printf("Grade E");
+    }
+    else if(p<40)
+    {
+        printf("Grade F");
+    }
+    else
+    {
+        printf("1");
+    }
+}
