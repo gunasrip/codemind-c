e929a7fb386672ef93f49df4686b4e49f2943eb9
gunasrip <119730782+gunasrip@users.noreply.github.com>
2023-05-23 18:49:01 +0530

    #include<stdio.h> int main() {     int n,rem,rev=0;     scanf("%d" ,&n);     while(n!=0)     {         rem = n%10;         rev = rev*10+rem;         n = n/10;
        }
        printf("%d" ,rev);
        
    }

--- Reverse_Integer.c
+++ Reverse_Integer.c
@@ -1,20 +1,15 @@
 #include<stdio.h>
-int is_palndrm(int);
 int main()
 {
-	int n;
-	scanf("%d",&n);
-	printf("%d",is_palndrm(n));	
-}
-int is_palndrm(int m)
-{
-	int q,r,s=0;
-	q=m;
-	while(q!=0)
-	{
-		r=q%10;
-		s=s*10+r;
-		q=q/10;
-	}
-	return s;
+    int n,rem,rev=0;
+    scanf("%d" ,&n);
+    while(n!=0)
+    {
+        rem = n%10;
+        rev = rev*10+rem;
+        n = n/10; 
+        
+    }
+    printf("%d" ,rev);
+    
 }
