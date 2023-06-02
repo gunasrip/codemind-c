610d52d135659a4bcc4baab62e936ab22213df73
gunasrip <119730782+gunasrip@users.noreply.github.com>
2023-06-02 15:21:18 +0530

    #include<stdio.h> int main() {     int n,a=0,b=1,x=0,y=1,c,z,w,m;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         if(a>n)         {             w=c-a;             break;             }             c=a+b;             a=b;             b=c;
        } 
        for(int j=1;j<=n;j++)
        { 
            if(x>n)
            { 
                m=x;
                break;
                } 
                //printf("%d",x);
                z=x+y;
                x=y;
                y=z;
                } 
                int d1=n-w;
                int d2=m-n; 
                if(d1<d2)
                {
                    printf("%d",w);
                    } 
                    else if(d2<d1)
                    { 
                        printf("%d",m);
                        }
                        else if(d1==d2) 
                        {
                            printf("%d %d",w,m);
                            }}

--- nearest_fibonacci.c
+++ nearest_fibonacci.c
@@ -1,23 +1,43 @@
 #include<stdio.h>
-#include<math.h>
 int main()
-{
-    int n,a=0,b=1,c,s=0;
-    scanf("%d",&n);
-    c=a+b;
-    while(c<=n)
+{ 
+    int n,a=0,b=1,x=0,y=1,c,z,w,m;
+    scanf("%d",&n); 
+    for(int i=1;i<=n;i++)
     {
-        a=b;
-        b=c;
-        c=a+b;
-    }
-    s=(abs(c-n)>=abs(b-n))?b:c;
-    if((abs(c-n)==abs(b-n)))
-    {
-        printf("%d %d",b,c);
-    }
-    else
-    {
-        printf("%d",s);
-    }
-}
+        if(a>n) 
+        {
+            w=c-a;
+            break;
+            }
+            c=a+b; 
+            a=b;
+            b=c; 
+        
+    } 
+    for(int j=1;j<=n;j++)
+    { 
+        if(x>n)
+        { 
+            m=x;
+            break;
+            } 
+            //printf("%d",x);
+            z=x+y;
+            x=y;
+            y=z;
+            } 
+            int d1=n-w;
+            int d2=m-n; 
+            if(d1<d2)
+            {
+                printf("%d",w);
+                } 
+                else if(d2<d1)
+                { 
+                    printf("%d",m);
+                    }
+                    else if(d1==d2) 
+                    {
+                        printf("%d %d",w,m);
+                        }}
